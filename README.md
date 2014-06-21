8bfc85a702f26337a23e83f15c02149035dec12e
black-night <black_night@gmx.net>
2014-06-21 12:28:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+mozilo-2.0-News
+======================
+Plugin für moziloCMS 2.0 (https://github.com/mozilo/mozilo2.0)
+Zur einfachen Darstellung von Newsbeiträgen.
+
+
+* Lizenz: 
+      * LGPL 3.0 (http://www.gnu.org/licenses/lgpl) (Deutsch: http://www.gnu.de/documents/lgpl-3.0.de.html)
+      * enthält HTML5 parser and serializer for PHP (https://github.com/Masterminds/html5-php Licenc MIT)
---
+++ index.php
@@ -0,0 +1,144 @@
+<?php if(!defined('IS_CMS')) die();
+require_once BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5.php';
+foreach (glob(BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/*.php') as $filename) {require_once $filename;}
+require_once BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/Parser/InputStream.php';
+require_once BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/Parser/EventHandler.php';
+require_once BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/Parser/StringInputStream.php';
+foreach (glob(BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/Parser/*.php') as $filename) {require_once $filename;}
+require_once BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/Serializer/RulesInterface.php';
+foreach (glob(BASE_DIR.PLUGIN_DIR_NAME.'/News/HTML5Parser/HTML5/Serializer/*.php') as $filename) {require_once $filename;}
+use Masterminds\HTML5;
+
+/***************************************************************
+ *
+* Plugin fuer moziloCMS, welches die Darstellung von News ermoeglicht
+* by black-night - Daniel Neef
+*
+***************************************************************/
+class News extends Plugin {
+    
+    private $lang_admin;
+    private $lang_cms;
+        
+    function getContent($value) {
+      global $CMS_CONF;
+      $this->lang_cms = new Language($this->PLUGIN_SELF_DIR."sprachen/cms_language_".$CMS_CONF->get("cmslanguage").".txt");
+      $default = array('text' => '', 'date' => '', 'title' => '', 'show' => '-1', 'site' => '');
+      $values = $this->makeUserParaArray($this->mapParams($value),$default,'|');
+      if ($values['show'] == -1) {
+          return $this->getHTMLNewsDef($values);
+      }else{ 
+          return $this->getHTMLNewsShow($values);
+      }
+      //return '<div class="DemoPlugin">'.$this->settings->get("text").' <span style="color:#'.$values['Farbe'].'">'.$values['Text'].'</span></div>';
+    }
+    function getConfig() {
+        $config = array();
+        $config['previewlength'] = array(
+                "type" => "text",
+                "description" => $this->lang_admin->getLanguageValue("config_previewlength"),
+                "maxlength" => "4",
+                "regex" => "/^[1-9][0-9]?/",
+                "regex_error" => $this->lang_admin->getLanguageValue("config_fnumber_regex_error")
+        );
+        $config['readon'] = array(
+                "type" => "text",
+                "description" => $this->lang_admin->getLanguageValue("config_readon"),
+                "maxlength" => "50",
+        );
+        return $config;
+    }
+    function getInfo() {
+        global $ADMIN_CONF;
+        $this->lang_admin = new Language($this->PLUGIN_SELF_DIR."sprachen/admin_language_".$ADMIN_CONF->get("language").".txt");
+        $info = array(
+            // Plugin-Name (wird in der Pluginübersicht im Adminbereich angezeigt)
+            $this->lang_admin->getLanguageValue("plugin_name")." \$Revision: 1 $",
+            // CMS-Version
+            "2.0",
+            // Kurzbeschreibung
+            $this->lang_admin->getLanguageValue("plugin_desc"),
+            // Name des Autors
+            "black-night",
+            // Download-URL
+            array("http://software.black-night.org","Software by black-night"),
+            // Platzhalter => Kurzbeschreibung, für Inhaltseditor
+            array('{News|title=...|date=...|text=...}' => $this->lang_admin->getLanguageValue("plugin_news_def"),
+                  '{News|show=...|site=...}' => $this->lang_admin->getLanguageValue("plugin_news_show"))
+            );
+        return $info;
+    }
+    private function mapParams($value) {
+        $result = $value;
+        $result = str_ireplace('|titel=','|title=',$result);
+        $result = str_ireplace('|datum=','|date=',$result);
+        $result = str_ireplace('|zeige=','|show=',$result);
+        $result = str_ireplace('|seite=','|site=',$result);
+        return $result;
+    }
+    private function getHTMLNewsDef($values) {
+        $result = '<article class="bnnews-def" id="bnnews'.str_replace(' ','',$values['title']).'">';
+        if ($values['title'] != '') {
+            $result .= '<header class="bnnews-header">'.$values['title'].'</header>';
+        }
+        if ($values['date'] != '') {
+            $result .= '<time class="bnnews-date">'.$values['date'].'</time>';
+        }
+        if ($values['text'] != '') {
+            $result .= '<div class="bnnews-text">'.$values['text'].'</div>';
+        }        
+        $result .= '</article>';
+        return $result;
+    }
+    private function getHTMLNewsShow($values) {
+        global $CatPage;
+        $result = '<article class="bnnews-show">';
+        list($cat,$page) = $CatPage->split_CatPage_fromSyntax($values['site']);
+        $html = $CatPage->get_pagecontent($cat,$page,true,true);
+        $html5 = new HTML5();
+        $dom = $html5->loadHTML($html);
+        $article = $dom->getElementsByTagName('article')->item($values['show']-1);
+        if ($article != NULL) {
+            $children = $article->childNodes;
+            $titleid = '';
+            foreach ($children as $child)
+            {
+                if ($child->attributes->getNamedItem('class')->value == 'bnnews-header') {
+                    $titleid = '#bnnews'.str_replace(' ','',$child->textContent);
+                }
+                if($child->attributes->getNamedItem('class')->value == 'bnnews-text') {
+                    $text = substr($child->textContent,0,$this->getPreviewLength())."...";
+                    $text_ende = strrchr($text, " ");
+                    $link = "... <a href=\"".$CatPage->get_Href($cat,$page,$titleid)."\">".$this->getReadOn()."</a>";
+                    $result .= '<div class="bnnews-text">'.str_replace($text_ende,$link, $text).'</div>';
+                }else{
+                    $result .= $child->ownerDocument->saveXML( $child );
+                }
+            }
+        }
+        $result .= '</article>';
+        return $result;
+    }
+    private function getInteger($value) {
+        if (is_numeric($value) and ($value > 0)) {
+            return $value;
+        } else {
+            return 1;
+        }
+    }    
+    private function getPreviewLength() {
+        if ($this->settings->get("previewlength")) {
+            return $this->getInteger($this->settings->get("previewlength"));
+        } else {
+            return 300;
+        }
+    }
+    private function getReadOn() {
+        if ($this->settings->get("readon")) {
+            return $this->settings->get("readon");
+        } else {
+            return 'weiterlesen';
+        }
+    }    
+  }
+?>
